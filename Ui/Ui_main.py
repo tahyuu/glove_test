2e57fcd9f9c43ee764013ab7b56eecbbe107590e
yongtan <tahyuu@hotmail.com>
2019-07-15 07:07:29 -0700

    update ui

--- Ui/Ui_main.py
+++ Ui/Ui_main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'E:\WorkSpace\gloves_test\Program\Ui\main.ui'
+# Form implementation generated from reading ui file 'C:\Users\yong\Desktop\glove_test\main.ui'
 #
 # Created by: PyQt4 UI code generator 4.11.4
 #
@@ -25,27 +25,9 @@ except AttributeError:
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(937, 699)
+        MainWindow.resize(800, 600)
         self.centralWidget = QtGui.QWidget(MainWindow)
         self.centralWidget.setObjectName(_fromUtf8("centralWidget"))
-        self.groupBox = QtGui.QGroupBox(self.centralWidget)
-        self.groupBox.setGeometry(QtCore.QRect(40, 30, 261, 641))
-        self.groupBox.setObjectName(_fromUtf8("groupBox"))
-        self.lineEdit = QtGui.QLineEdit(self.groupBox)
-        self.lineEdit.setGeometry(QtCore.QRect(60, 60, 113, 20))
-        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
-        self.groupBox_2 = QtGui.QGroupBox(self.centralWidget)
-        self.groupBox_2.setGeometry(QtCore.QRect(340, 30, 261, 641))
-        self.groupBox_2.setObjectName(_fromUtf8("groupBox_2"))
-        self.lineEdit_2 = QtGui.QLineEdit(self.groupBox_2)
-        self.lineEdit_2.setGeometry(QtCore.QRect(70, 40, 113, 20))
-        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
-        self.groupBox_3 = QtGui.QGroupBox(self.centralWidget)
-        self.groupBox_3.setGeometry(QtCore.QRect(640, 30, 261, 641))
-        self.groupBox_3.setObjectName(_fromUtf8("groupBox_3"))
-        self.lineEdit_3 = QtGui.QLineEdit(self.groupBox_3)
-        self.lineEdit_3.setGeometry(QtCore.QRect(70, 40, 113, 20))
-        self.lineEdit_3.setObjectName(_fromUtf8("lineEdit_3"))
         MainWindow.setCentralWidget(self.centralWidget)
 
         self.retranslateUi(MainWindow)
@@ -53,9 +35,6 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
-        self.groupBox.setTitle(_translate("MainWindow", "Sample One", None))
-        self.groupBox_2.setTitle(_translate("MainWindow", "Sample two", None))
-        self.groupBox_3.setTitle(_translate("MainWindow", "Sample two", None))
 
 
 if __name__ == "__main__":
---
+++ Ui/Ui_sample_select.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\yong\Desktop\glove_test\sample_select.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_sample_select(object):
+    def setupUi(self, sample_select):
+        sample_select.setObjectName(_fromUtf8("sample_select"))
+        sample_select.resize(427, 128)
+        sample_select.setSizeGripEnabled(True)
+        self.checkBox = QtGui.QCheckBox(sample_select)
+        self.checkBox.setGeometry(QtCore.QRect(40, 30, 81, 61))
+        self.checkBox.setObjectName(_fromUtf8("checkBox"))
+        self.checkBox_2 = QtGui.QCheckBox(sample_select)
+        self.checkBox_2.setGeometry(QtCore.QRect(150, 30, 81, 61))
+        self.checkBox_2.setObjectName(_fromUtf8("checkBox_2"))
+        self.checkBox_3 = QtGui.QCheckBox(sample_select)
+        self.checkBox_3.setGeometry(QtCore.QRect(250, 30, 81, 61))
+        self.checkBox_3.setObjectName(_fromUtf8("checkBox_3"))
+        self.next_step = QtGui.QPushButton(sample_select)
+        self.next_step.setGeometry(QtCore.QRect(310, 90, 93, 28))
+        self.next_step.setObjectName(_fromUtf8("next_step"))
+        self.label = QtGui.QLabel(sample_select)
+        self.label.setGeometry(QtCore.QRect(10, 10, 171, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label.setFont(font)
+        self.label.setObjectName(_fromUtf8("label"))
+
+        self.retranslateUi(sample_select)
+        QtCore.QObject.connect(self.next_step, QtCore.SIGNAL(_fromUtf8("pressed()")), sample_select.close)
+        QtCore.QMetaObject.connectSlotsByName(sample_select)
+
+    def retranslateUi(self, sample_select):
+        sample_select.setWindowTitle(_translate("sample_select", "Sample Slect", None))
+        self.checkBox.setText(_translate("sample_select", "Sample1", None))
+        self.checkBox_2.setText(_translate("sample_select", "Sample2", None))
+        self.checkBox_3.setText(_translate("sample_select", "Sample2", None))
+        self.next_step.setText(_translate("sample_select", "Next Step", None))
+        self.label.setText(_translate("sample_select", "Step1:  Select test samples", None))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    sample_select = QtGui.QDialog()
+    ui = Ui_sample_select()
+    ui.setupUi(sample_select)
+    sample_select.show()
+    sys.exit(app.exec_())
+
